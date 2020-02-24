60c82b4119ea7625804ea21b5600bff479bf37f2
zcong1993 <zhangcong1992@gmail.com>
2020-02-25 01:30:45 +0800

    chore: add btso config

--- getConfig.js
+++ getConfig.js
@@ -1,16 +1,18 @@
-import { callAvmooApi, callAvsoxApi, callAvmemoApi } from './api'
+import { callAvmooApi, callAvsoxApi, callAvmemoApi, callBtsoApi } from './api'
 
 export const getAll = async () => {
-  const [avmoo, avsox, avmemo] = await Promise.all([
+  const [avmoo, avsox, avmemo, btso] = await Promise.all([
     callAvmooApi(),
     callAvsoxApi(),
     callAvmemoApi(),
+    callBtsoApi(),
   ])
 
   return {
     latest_version: '2.2.8',
     latest_version_code: 13,
     changelog: '更新配置地址\nbtso 地址使用网络地址',
+    btso_url: btso,
     data_sources: [
       {
         name: 'AVMOO 日本',
